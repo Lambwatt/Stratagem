5b455d47b8e7fc994cbf7b31e49051b2d651e2ca
Michael Pattie <mjpattie@Michaels-MacBook-Air.local>
2013-10-18 00:52:53 -0700

    started adding test event. Collection's don't work as expected. Build a LL instead.

--- front_end_0.2/eventManager.js
+++ front_end_0.2/eventManager.js
@@ -1,12 +1,52 @@
 //contains queue of changes to be made to the model on a fixed rate. (4x/sec?) contains code to manage event system.
 
 var eventManager = new EventManager();
+
+function countTo3(){
+
+	var stEvent = new function(){
+		this.value;
+		
+		this.init = function(){
+			this.value = 0;
+		}
+
+		this.step = function(){
+			this.value++;
+			return this.value>=3;
+		}
+
+		this.end = function(){
+			alert("param value = "+this.value);
+		}
+	}
+}
+
+var countTo3Function = new StEvent([], countTo3.init, countTo3.step, countTo3.end);
+
+var countTo3Param = new StEvent([],
+		 function(){
+			this.value = 0;
+		},
+		function(){
+			this.value++;
+			return this.value>=3;
+		},
+		 function(){
+			alert("func value = "+this.value);
+		});
+
+eventManager.addEvent(countTo3Function);
+eventManager.addEvent(countTo3Param);
+
 setInterval(function() {
     //var startTime = (new Date()).getTime();
     eventManager.chechPendingEvents();
 		eventManager.step();
 		eventManager.finish();
 }, 10);
+
+
 
 function StEvent(prereqs, init_code, step_code, end_code){
 	this.prerqs = prereqs;
@@ -15,7 +55,7 @@ function StEvent(prereqs, init_code, step_code, end_code){
 	this.end_code = end_code;
 
 	//Checks that all prereq flags are true.
-	this.checkPreqs = function{
+	this.checkPreqs = function(){
 		for(var i=0; i<this.prereqs.length; i++){
 			if(!this.prereqs[i])
 				return false;
@@ -24,8 +64,8 @@ function StEvent(prereqs, init_code, step_code, end_code){
 	}
 
 	//run the start code exactly
-	this.start = function()
-		this.start_code();
+	this.start = function(){
+		this.init_code();
 	}
 
 	//run the step code many times. step code returnes completeness
@@ -76,4 +116,37 @@ function EventManager(){
 	}
 }
 
-//set interval.
+function LinkedList(){
+	
+	function Node(){
+		this.next = null;
+		this.contents = null;
+	}
+
+	this.head = null;
+
+	this.push = function(x){
+		if(this.head==null){
+			this.head = new Node();
+			this.head.contents = x;
+		}
+		else
+		{
+			var tmp = new Node();
+			tmp.contents = x;
+			tmp.next = this.head;
+			this.head = tmp;
+		}
+	}
+
+	this.remove = function(x){
+		var tmp = head;i
+		//FINISH ME
+		if()
+		while(tmp!=null){
+			if(tmp.contents == x){
+				
+			}
+		}
+	}
+}
--- front_end_0.2/events.js
+++ front_end_0.2/events.js
@@ -8,3 +8,37 @@ function moveUnitOneSquare(unit, dir){
 	function(){});
 	eventManager.add(stEvent);
 }
+
+function countTo3(){
+
+	var stEvent = new function(){
+		this.value;
+		
+		this.init = function(){
+			this.value = 0;
+		}
+
+		this.step = function(){
+			this.value++;
+			return this.value>=3;
+		}
+
+		this.end = function(){
+			alert("value = "+this.value);
+		}
+	}
+}
+
+var countTo3Function = new STEvent([], countTo3.init, countTo3.step, countTo3.end);
+
+var countTo3Param = new STEvent([],
+		 function(){
+			this.value = 0;
+		},
+		function(){
+			this.value++;
+			return this.value>=3;
+		},
+		 function(){
+			alert("value = "+this.value);
+		});
